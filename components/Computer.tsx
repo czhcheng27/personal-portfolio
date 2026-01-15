6462cd3f20f1e49025dae1b4c1054d52f4d6612c
chengzihang <czhcheng27@gmail.com>
2026-01-16 07:17:00 +0800

    Computer Component

--- components/Computer.tsx
+++ components/Computer.tsx
@@ -1,13 +1,11 @@
-"use client";
-
-import { Suspense, useEffect, useState } from "react";
+import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-import CanvasLoader from "../Loader";
+import CanvasLoader from "./Loader";
 
 interface ComputersCanvasProps {}
 
-const Computers = ({ isMobile }: { isMobile: boolean }) => {
+const Computers = () => {
   // load from public folder via absolute path
   const computer = useGLTF("/desktop_pc/scene.gltf");
 
@@ -28,8 +26,8 @@ const Computers = ({ isMobile }: { isMobile: boolean }) => {
       <directionalLight position={[-5, 1, 5]} intensity={2} castShadow />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        scale={0.75}
+        position={[-1, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -37,31 +35,6 @@ const Computers = ({ isMobile }: { isMobile: boolean }) => {
 };
 
 const ComputersCanvas = ({}: ComputersCanvasProps) => {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    // Add a listener for changes to the screen size
-    const mediaQuery = window.matchMedia("(max-width: 500px)");
-
-    // Set the initial value of the `isMobile` state variable
-    setIsMobile(mediaQuery.matches);
-
-    // Define a callback function to handle changes to the media query
-    const handleMediaQueryChange = (event: {
-      matches: boolean | ((prevState: boolean) => boolean);
-    }) => {
-      setIsMobile(event.matches);
-    };
-
-    // Add the callback function as a listener for changes to the media query
-    mediaQuery.addEventListener("change", handleMediaQueryChange);
-
-    // Remove the listener when the component is unmounted
-    return () => {
-      mediaQuery.removeEventListener("change", handleMediaQueryChange);
-    };
-  }, []);
-
   return (
     <Canvas
       frameloop="demand"
@@ -76,7 +49,7 @@ const ComputersCanvas = ({}: ComputersCanvasProps) => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers isMobile={isMobile} />
+        <Computers />
       </Suspense>
 
       <Preload all />

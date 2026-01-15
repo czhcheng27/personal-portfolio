1c48c3b6af1a1874da50b6945b2ec44efe6c6e8c
chengzihang <czhcheng27@gmail.com>
2026-01-16 07:16:45 +0800

    Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
+import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='relative z-0 bg-primary'>
+      <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
+        </div>
+    </div>
   );
 }
